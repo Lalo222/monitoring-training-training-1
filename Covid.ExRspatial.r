783925f02f365d501bb7b0e95493ab39330b5f2c
Lalo222 <73694015+Lalo222@users.noreply.github.com>
2020-11-16 10:47:00 +0100

    Update Covid.ExRspatial.r

--- Covid.ExRspatial.r
+++ Covid.ExRspatial.r
@@ -33,7 +33,10 @@ spplot(meuse,"copper",main="Concentration of copper")
 # Exercise, see Cu and Zn in two panels, good, we can compare data
 
 #tried to solve  spplotplot("copper","zinc")
-#Error in spplotplot("copper", "zinc") : 
+#Error in spplotplot("copper", "zinc") :
+
+#how to make the command to put the path
+
   #could not find function "spplotplot"  because I need to define my data meuse,c(
 
 #ok, do thi,slmost I did not put the c!!!!  
@@ -62,5 +65,68 @@ C:\Users\123fatto\Desktop\R code
 
 
 #downloaded Covid data
+#install ggplot2 and recall with library
+
+#install.packages("ggplot2")
+#we will build the dataframe, var 1, 2, etc.
+
+#ecological data frame airplanes, 
+#biofuels as amount of fossil fuel to be reduced using biofuels
+
+biofuels <-c(120, 200, 350, 570, 750)   #built this set cosi, n1, n2, n3 etc. and grouped array:use c  will be first column
+#oxidative enzymes
+oxidative <-c(1200, 1300, 21000, 34000, 50000)  #will be second column  biofuels    oxidative
+#then type name of set to recall and make a frame with [1]
+
+#function to build dataframe is data.frame
+
+#data.frame  for biofuels,    oxidative
+d <- data.frame(biofuels, oxidative)
+#then d to call it up, so always function with name then use name to use it, 
+#spaces don't matter here
+#so far: > library(ggplot2)
+> biofuels <-c(120, 200, 350, 570, 750)
+> biofuels
+[1] 120 200 350 570 750
+> oxidative <-c(1200, 1300, 21000, 34000, 50000)
+> oxidative
+[1]  1200  1300 21000 34000 50000
+> d <- data.frame(biofuels, oxidative)
+> d
+  biofuels oxidative
+1      120      1200
+2      200      1300
+3      350     21000
+4      570     34000
+5      750     50000
+
+#now plot data, use plot but here ggplot2 is ggplot
+
+#ggplot(data = ,aes(),..,###=parent frame) -- aestetics..aes
+ggplot(d, aes(x=biofuels, y=oxidative))  #this opens empty window ready,we have defined the parameters of the "room"
+#explain to R we are adding an additional parent, geometry  + geom(choose type we want, point for scatter)
+ggplot(d, aes(x=biofuels, y=oxidative)) + geom_point()
+
+#gave a nice plot with labels for axis of how the values are related
+#now change colors ggplot(d, aes(x=biofuels, y=oxidative)) + geom_point(size=5, col="red")
+ggplot(d, aes(x=biofuels, y=oxidative)) + geom_point(size=5, col= "red")
+
+#lines  use geom_line ...cool
+ggplot(d, aes(x=biofuels, y=oxidative)) + geom_line(size=5, col="red")
+
+#now points and lines geom_point + geom_line together
+# could not change colors for both ggplot(d, aes(x=biofuels, y=oxidative)) + geom_point(size=5,col="red") + geom_line(size 7, col="blue")
+ggplot(d, aes(x=biofuels, y=oxidative)) + geom_point(size=5, col="red")
+
+#geom_polygon if we want
+
+#setwd, for importing and exporting data so build new directory called lab
+
+#set for windows, setwd("")
+setwd("C:/
+
+
+
+
 
  
