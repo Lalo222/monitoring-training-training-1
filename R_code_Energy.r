0c08f398fd1b19a94138e7d9a2628e5413da0cc8
Lalo222 <73694015+Lalo222@users.noreply.github.com>
2021-01-17 19:43:56 +0100

    Create R_code_Energy.r

---
+++ R_code_Energy.r
@@ -0,0 +1,47 @@
+#R_code_Energy.r
+#His student in the Rcran for Rasterdiv
+
+#Install Rasterdiv 
+#We see data from Copernicus, NDVI
+install.packages("rasterdiv)
+library(rasterdiv)
+library(raster)
+library(rasterVis)  #All good
+
+## His code 10-46
+# R_code_energy.r
+
+# install.packages("rasterdiv")
+# iinstall.packages("rasterVis")
+library(rasterdiv)
+library(raster)
+library(rasterVis)
+
+data(copNDVI)
+plot(copNDVI)
+
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+plot(copNDVI)
+
+levelplot(copNDVI)
+
+
+clymin <- colorRampPalette(c('yellow','red','blue'))(100) # 
+plot(copNDVI, col=clymin)
+
+clymed <- colorRampPalette(c('red','yellow','blue'))(100) # 
+plot(copNDVI, col=clymed)
+
+clymax <- colorRampPalette(c('blue','red','yellow'))(100) # 
+plot(copNDVI, col=clymax)
+
+par(mfrow=c(1,2))
+clymed <- colorRampPalette(c('red','yellow','blue'))(100) # 
+plot(copNDVI, col=clymed)
+clymax <- colorRampPalette(c('blue','red','yellow'))(100) # 
+plot(copNDVI, col=clymax)
+
+# crop
+ext <- c(0,20,35,55)  # xmin xmax ymin ymax
+copNDVI_Italy <- crop(copNDVI, ext)
+plot(copNDVI_Italy, col=clymax
